ae3ce730d8dd1b77c40ddac3f52a9e4fba798ed5
utas-raymondng <manfai.ng@utas.edu.au>
2023-02-17 09:55:48 +1100

    fix bug on copy path

--- aodn.js
+++ aodn.js
@@ -1,5 +1,17 @@
 // Add on functions to customize the s3 explorer
+const getParams = (url) => {
 
+  let queryString = url.substring(url.indexOf('?') + 1);
+  let paramsArr = queryString.split('&');
+  let params = {};
+
+  for (let i = 0, len = paramsArr.length; i < len; i++) {
+    let keyValuePair = paramsArr[i].split('=');
+    params[keyValuePair[0]] = keyValuePair[1];
+  }
+
+  return params;
+}
 /**
  * Copy to clipboard using navigator only works if user init https connection.
  * If not then you need to use an obsolete command execCommand('copy')
@@ -45,6 +57,16 @@ const copyToClipboard = (url) => {
     .then(() => showLinkCopyDialog('#link-copy-success'))
     .catch(()=>  showLinkCopyDialog('#link-copy-fail'));
 }
+
+const object2href = (bucket, key) => {
+  var enckey = key.split('/').map(function(x) { return encodeURIComponent(x); }).join('/');
+
+  if (isfolder(key)) {
+    return document.location.origin + '/index.html?p=' + enckey;
+  } else {
+    return document.location.origin + '/' + enckey;
+  }
+}
 /**
  * Return an HTML that represent the icons for a particular row.
  * @param data
@@ -53,6 +75,6 @@ const copyToClipboard = (url) => {
  * @returns {string}
  */
 const displayIcon = (data, type, full) => {
-  let url = object2hrefvirt(s3exp_config.Bucket, data);
+  let url = object2href(s3exp_config.Bucket, data);
   return '<button onclick="copyToClipboard(\'' + url + '\')" class="bi-share-fill"/>';
 }
